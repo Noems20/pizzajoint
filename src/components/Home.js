81543ac2f65d144fa32aa0af129a11ba38f18486
Noé Muñoz Sánchez <noesm.16@gmail.com>
2021-07-13 03:15:31 -0500

    Added several animations on the page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,19 +1,59 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { motion } from 'framer-motion';
 
 const Home = () => {
+  const buttonVariants = {
+    // visible: {
+    //   x: ['0px', '-20px', '20px', '-20px', '20px', '0px'],  ------------- KEYFRAMES
+    //   transition: { delay: 2 },
+    // },
+    hover: {
+      scale: 1.1,
+      textShadow: '0px 0px 8px rgb(255,255,255)',
+      boxShadow: '0px 0px 8px rgb(255,255,255)',
+      transition: {
+        duration: 0.3,
+        yoyo: Infinity,
+      },
+    },
+  };
+
+  const containerVariants = {
+    hidden: {
+      opacity: 0,
+    },
+    visible: {
+      opacity: 1,
+      transition: {
+        delay: 0.5,
+        duration: 1.5,
+      },
+    },
+    exit: {
+      x: '-100vw',
+      transition: {
+        transition: { ease: 'easeInOut' },
+      },
+    },
+  };
+
   return (
-    <div className="home container">
-      <h2>
-        Welcome to Pizza Joint
-      </h2>
-      <Link to="/base">
-        <button>
+    <motion.div
+      className='home container'
+      variants={containerVariants}
+      initial='hidden'
+      animate='visible'
+      exit='exit'
+    >
+      <h2>Welcome to Pizza Joint</h2>
+      <Link to='/base'>
+        <motion.button variants={buttonVariants} whileHover='hover'>
           Create Your Pizza
-        </button>
+        </motion.button>
       </Link>
-    </div>
-  )
-}
+    </motion.div>
+  );
+};
 
 export default Home;
--- src/components/Order.js
+++ src/components/Order.js
@@ -1,13 +1,73 @@
+import { motion } from 'framer-motion';
 import React from 'react';
 
 const Order = ({ pizza }) => {
+  const containerVariants = {
+    hidden: {
+      opacity: 0,
+      x: '100vw',
+    },
+    visible: {
+      opacity: 1,
+      x: 0,
+      transition: {
+        type: 'spring',
+        mass: 0.4,
+        damping: 8,
+        when: 'beforeChildren',
+        staggerChildren: 0.4,
+      },
+    },
+  };
+
+  const childVariants = {
+    hidden: {
+      opacity: 0,
+    },
+    visible: {
+      opacity: 1,
+      transition: {
+        staggerChildren: 0.1,
+      },
+    },
+  };
+
+  const childChildVariants = {
+    hidden: {
+      opacity: 0,
+      x: '100vw',
+    },
+    visible: {
+      opacity: 1,
+      x: 0,
+      transition: {
+        type: 'tween',
+        // stiffness: 500,
+        duration: 1,
+      },
+    },
+  };
+
   return (
-    <div className="container order">
+    <motion.div
+      className='container order'
+      variants={containerVariants}
+      initial='hidden'
+      animate='visible'
+    >
       <h2>Thank you for your order :)</h2>
-      <p>You ordered a {pizza.base} pizza with:</p>
-      {pizza.toppings.map(topping => <div key={topping}>{topping}</div>)}
-    </div>
-  )
-}
+      <motion.p variants={childVariants}>
+        You ordered a {pizza.base} pizza with:
+      </motion.p>
+      <motion.div variants={childVariants}>
+        {pizza.toppings.map((topping) => (
+          <motion.div key={topping} variants={childChildVariants}>
+            {topping}
+          </motion.div>
+        ))}
+      </motion.div>
+    </motion.div>
+  );
+};
 
 export default Order;
